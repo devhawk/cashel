09c4461433a5dfba0037ea9dc08ca748f1b7c3e7
Ryan Riley <ryan.riley@panesofglass.org>
2013-04-12 09:57:32 -0500

    Remove binary dependencies and leverage FAKE boot to load FAKE only once.

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cls
-".nuget/nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion" "-Prerelease"
-".nuget/nuget.exe" "install" "NUnit.Runners" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools/FAKE/tools/Fake.exe" "build.fsx" %*
-pause
-
